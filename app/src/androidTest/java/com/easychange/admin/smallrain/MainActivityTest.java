3f71f89cafc743f9d6117e42fdc73eaea0881a15
ningshan <ningshan@daokoudai.com>
2019-04-26 19:10:42 +0800

    添加test包到git

---
+++ app/src/androidTest/java/com/easychange/admin/smallrain/MainActivityTest.java
@@ -0,0 +1,25 @@
+package com.easychange.admin.smallrain;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+
+public class MainActivityTest {
+
+    //默认在测试之前启动该Activity
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+
+}
