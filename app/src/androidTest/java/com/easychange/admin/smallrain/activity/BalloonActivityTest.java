2d2b6cf6dfbe8708ceeba1d34a89ff05920c2dbb
ningshan <ningshan@daokoudai.com>
2019-04-30 14:39:16 +0800

    测试用例4

--- app/src/androidTest/java/com/easychange/admin/smallrain/activity/BalloonActivityTest.java
+++ app/src/androidTest/java/com/easychange/admin/smallrain/activity/BalloonActivityTest.java
@@ -2,8 +2,15 @@ package com.easychange.admin.smallrain.activity;
 
 import android.support.test.rule.ActivityTestRule;
 
+import com.easychange.admin.smallrain.entity.NetChangeBean;
+import com.easychange.admin.smallrain.testN.FuncUtils;
+
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.lang.reflect.Method;
+
+import bean.ChildMessageBean;
 
 public class BalloonActivityTest {
     //默认在测试之前启动该Activity
@@ -14,5 +21,22 @@ public class BalloonActivityTest {
     @Test
     public void test() {
         mActivityRule.getActivity().test();
+
+        NetChangeBean bean = new NetChangeBean(false);
+        Method BreakNetBean = FuncUtils.funcMethod(BalloonActivity.class,
+                "BreakNetBean", NetChangeBean.class);
+        FuncUtils.funcInvoke(mActivityRule.getActivity(),
+                BreakNetBean, bean);
+
+        Method showPingguDialog = FuncUtils.funcMethod(BalloonActivity.class,
+                "showPingguDialog");
+        FuncUtils.funcInvoke(mActivityRule.getActivity(),
+                showPingguDialog);
+
+        ChildMessageBean childMessageBean = new ChildMessageBean();
+        Method setPicNameFromWeb = FuncUtils.funcMethod(BalloonActivity.class,
+                "setPicNameFromWeb", ChildMessageBean.class);
+        FuncUtils.funcInvoke(mActivityRule.getActivity(),
+                setPicNameFromWeb, childMessageBean);
     }
 }
--- app/src/androidTest/java/com/easychange/admin/smallrain/activity/BalloonExperienceActivityTest.java
+++ app/src/androidTest/java/com/easychange/admin/smallrain/activity/BalloonExperienceActivityTest.java
@@ -2,8 +2,13 @@ package com.easychange.admin.smallrain.activity;
 
 import android.support.test.rule.ActivityTestRule;
 
+import com.easychange.admin.smallrain.entity.NetChangeBean;
+import com.easychange.admin.smallrain.testN.FuncUtils;
+
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.lang.reflect.Method;
 
 public class BalloonExperienceActivityTest {
     //默认在测试之前启动该Activity
@@ -14,5 +19,15 @@ public class BalloonExperienceActivityTest {
     @Test
     public void test() {
         mActivityRule.getActivity().test();
+
+        Method showIsRemindDialog = FuncUtils.funcMethod(BalloonExperienceActivity.class,
+                "showIsRemindDialog", String.class, String.class, String.class);
+        FuncUtils.funcInvoke(mActivityRule.getActivity(),
+                showIsRemindDialog, "dd", "ddd", "dddd");
+
+        Method showFamilyDialog = FuncUtils.funcMethod(BalloonExperienceActivity.class,
+                "showFamilyDialog");
+        FuncUtils.funcInvoke(mActivityRule.getActivity(),
+                showFamilyDialog);
     }
 }
